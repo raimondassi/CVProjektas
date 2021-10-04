3d5dcacf56a82dc637394c3bc64048fc1f4237e6
raimondassi <raimondassiup@gmail.com>
2021-10-04 18:28:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function reject(){
+    let emoji = String.fromCodePoint(0x1F621)
+    alert("You made a big mistake  " +emoji);
+
+}
+function approve(){
+    let emoji = String.fromCodePoint(0x1F600)
+    alert("Thank you your majesty "+emoji)
+
+}
+
